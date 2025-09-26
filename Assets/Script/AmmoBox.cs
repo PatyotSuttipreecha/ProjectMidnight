6459e7f9b6bd9b6f6d86037fb81a469e5e4938c4
PatyotSuttipreecha <yudo.22@hotmail.com>
2025-09-26 11:50:56 +0700

    Update
    - อัพเดท Script การเก็บกระสุน 9mm Ammo ให้ตรงกับ Weapon Type
    - เพิ่มเสียงปืน Pistol , Shotgun และ Ambient เข้าไปในเกม

--- Assets/Script/AmmoBox.cs
+++ Assets/Script/AmmoBox.cs
@@ -3,47 +3,62 @@ using UnityEngine;
 
 public class AmmoBox : MonoBehaviour
 {
+    [Header("AmmoSetting")]
+    public Guns.WeaponType weaponType;
+    public int ammoGive;
     [Header("InteractionText")]
     [SerializeField] private TMP_Text text;
 
     public bool isInArea;
-    public bool isPickup;
+
     private void Start()
     {
-        
         text.gameObject.SetActive(false);
     }
-    private void OnTriggerStay(Collider other)
+
+    private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player"))
         {
+            text.gameObject.SetActive(true);
             isInArea = true;
-            Pickup();
         }
     }
+
     private void OnTriggerExit(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             isInArea = false;
-            Pickup();
+            text.gameObject.SetActive(false);
         }
     }
 
+    private void Update()
+    {
+        // เช็คว่าอยู่ในพื้นที่เก็บได้ และกด F
+        if (isInArea && Input.GetKeyDown(KeyCode.F))
+        {
+            // TODO: เพิ่มระบบเพิ่ม Ammo ให้ Player ได้ตรงนี้
+            Pickup();
+            Debug.Log("Picked up Ammo!");
+        }
+    }
     void Pickup()
     {
-        if(isInArea)
+        Guns[] allGun = Resources.FindObjectsOfTypeAll<Guns>();
+
+        foreach (Guns gun in allGun)
         {
-            text.gameObject.SetActive(true);
-            if(Input.GetKeyDown(KeyCode.F))
+            if(gun.weaponStat.weaponName == weaponType)
             {
+                ammoGive = Random.Range(6, 12);
+                gun.weaponStat.ammoReserve += ammoGive;
                 Destroy(this.gameObject);
-                isInArea = false;
+                text.gameObject.SetActive(false);
+                break;
+                
             }
-        }
-        else
-        {
-            text.gameObject.SetActive(false);
         }
     }
 }
